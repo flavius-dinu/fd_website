d5d7b0d10dbc4579a9c59856677cd94e9e0cf221
Flavius <flaviuscdinu93@gmail.com>
2025-10-04 18:11:13 +0300

    Added more links to my profile

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,43 +4,51 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: <a href="https://techblog.flaviusdinu.com">Medium Blog Articles</a>,
+    title: 'Medium Blog Articles',
+    link: 'https://techblog.flaviusdinu.com',
     Svg: require('@site/static/img/medium-icon-svgrepo-com.svg').default,
-    // description: (
-    //   <>
-    //     Check out my Medium blog
-    //   </>
-    // ),
   },
   {
-    title: <a href='https://www.linkedin.com/in/flaviuscristiandinu/'>LinkedIn Profile</a>,
+    title: 'LinkedIn Profile',
+    link: 'https://www.linkedin.com/in/flaviuscristiandinu/',
     Svg: require('@site/static/img/LinkedIn_icon.svg').default,
-    // description: (
-    //   <>
-    //     LinkedIn profile
-    //   </>
-    // ),
   },
   {
-    title: <a href='https://spacelift.io/blog/author/flaviusd'>Spacelift Blog Articles</a>,
+    title: 'Spacelift Blog',
+    link: 'https://spacelift.io/blog/author/flaviusd',
     Svg: require('@site/static/img/Spacelift.svg').default,
-    // description: (
-    //   <>
-    //     Extend or customize your website layout by reusing React. Docusaurus can
-    //     be extended while reusing the same header and footer.
-    //   </>
-    // ),
+  },
+  {
+    title: 'YouTube',
+    link: 'https://www.youtube.com/@devopswithflavius',
+    Svg: require('@site/static/img/DevOps.svg').default,
+  },
+  {
+    title: 'K8sLens Blog',
+    link: 'https://k8slens.dev/blog',
+    Svg: require('@site/static/img/Lens.svg').default,
+  },
+  {
+    title: 'GitHub',
+    link: 'https://github.com/flavius-dinu',
+    Svg: require('@site/static/img/GitHub.svg').default,
   },
 ];
 
-function Feature({ Svg, title, description }) {
+function Feature({ Svg, title, link, description }) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          <Svg className={styles.featureSvg} role="img" />
+        </a>
       </div>
       <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
+        <Heading as="h3">
+          <a href={link} target="_blank" rel="noopener noreferrer">
+            {title}
+          </a>
+        </Heading>
         <p>{description}</p>
       </div>
     </div>
